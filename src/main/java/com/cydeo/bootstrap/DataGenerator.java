67df79ca0bb00a83f39b914c1782ca89649e39ef
efecagrisen <efecagrisen@yahoo.com>
2024-07-04 08:25:33 +0300

    Roles added to map (DB)

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,17 +1,33 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
 
-// note: as we don't have a DB layer right now, although the role object needs to be saved in the DB which must be entity object itself, we use dto objects.
+    private final RoleService roleService;
+
+    public DataGenerator(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    // note: as we don't have a DB layer right now, although the role object needs to be saved in the DB which must be entity object itself, we use dto objects.
     @Override
     public void run(String... args) throws Exception {
 
         RoleDTO adminRole = new RoleDTO(1L,"Admin");
         RoleDTO managerRole = new RoleDTO(2L,"Manager");
         RoleDTO employeeRole = new RoleDTO(3L,"Employee");
+
+        roleService.save(adminRole);
+        roleService.save(managerRole);
+        roleService.save(employeeRole);
+
+
+
 
     }
 }
