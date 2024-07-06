3a22d67766b9888093ee549cf55f2a7b1baeac9c
efecagrisen <efecagrisen@yahoo.com>
2024-07-06 08:24:16 +0300

    Save and Delete buttons completed

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -6,15 +6,5 @@ import java.util.List;
 
 public interface RoleService extends CrudService<RoleDTO,Long>{
 
-    @Override
-    RoleDTO save(RoleDTO object);
 
-    @Override
-    RoleDTO findById(Long id);
-
-    @Override
-    List<RoleDTO> findAll();
-
-    @Override
-    void deleteById(Long id);
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -3,19 +3,10 @@ package com.cydeo.service;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface UserService extends CrudService<UserDTO, String> {
 
-    @Override
-    UserDTO save(UserDTO object);
 
-    @Override
-    UserDTO findById(String id);
-
-    @Override
-    List<UserDTO> findAll();
-
-    @Override
-    void deleteById(String id);
 }
