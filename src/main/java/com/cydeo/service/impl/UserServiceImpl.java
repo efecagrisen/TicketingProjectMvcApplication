3a664a3229d864bdfc66f4db1d5da6988844de0f
efecagrisen <efecagrisen@yahoo.com>
2024-07-07 19:53:43 +0300

    part-4 completed

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -45,4 +45,9 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
 
         return findAll().stream().filter(managers->managers.getRole().getId()==2).collect(Collectors.toList());
     }
+
+    @Override
+    public List<UserDTO> findEmployees() {
+        return findAll().stream().filter(managers->managers.getRole().getId()==3).collect(Collectors.toList());
+    }
 }
